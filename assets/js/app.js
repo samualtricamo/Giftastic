929b4b6c51a4b127dfffb9b1395886fc4b8d3fd1
Sam Tricamo <samualtricamo@gmail.com>
2019-07-13 20:02:46 -0500

    functional js, html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,74 @@
-$(document).ready(function() {
+var topics = ["Leslie Knope", "Ron Swanson", "Andy Dwyer", "Ben Wyatt", "April Ludgate", "Tom Haverford",
+"Jerry Gergitch", "Chris Trager", "Donna Meagle", "Jean Ralphio"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG";
 
-    var topics = ["Michael", "Dwight", "Jim", "Pam", "Andy", "Kevin", "Stanley", "Angela", "Phyllis", "Creed"];
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("parks-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".parks-button").unbind("click");
 
-    function displayInfo();
-    $("#office-view").empty();
-    var topic = $("this).attr("data-name");
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + topic + '&api_key=jVz8LzEOLKTNPSj1tux4I40dP1PX6Pbg&limit=10';
+	$(".parks-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
 
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
 }
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#parks-show").val().trim());
+		$("#parks-show").val("");
+	});
+});
